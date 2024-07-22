c88097db4c7e0a1c33850ca6d86c62d3bdbc6ca3
ilyoung <ext3fs@hanmail.net>
2024-07-23 06:15:37 +0900

    Roter & Component & Scss Create

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import HomeView from './views/HomeView';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
