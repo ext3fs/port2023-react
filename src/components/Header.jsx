e00c2e6df62b4f906fb5ccac6e521d4fbf9a9914
ilyoung <ext3fs@hanmail.net>
2024-07-25 03:08:28 +0900

    Header.jsx complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const headerNav = [
+  {
+    id: 1,
+    title: 'intro',
+    link: '#intro',
+  },
+  {
+    id: 2,
+    title: 'skill',
+    link: '#skill',
+  },
+  {
+    id: 3,
+    title: 'site',
+    link: '#site',
+  },
+  {
+    id: 4,
+    title: 'portfolio',
+    link: '#port',
+  },
+  {
+    id: 5,
+    title: 'contact',
+    link: '#contact',
+  },
+]
 
 const Header = () => {
+  const [show, setShow] = useState(false);
+  
   return (
-    <div>
-      Header
-    </div>
+    <header id="header" role="banner">
+      <div className="header__inner">
+        <div className="header__logo">
+          <h1><a href="/">portfolio<em>react</em></a></h1>
+        </div>
+        <nav className={"header__nav" + (show ? " show" : "")} role="navigation" aria-label="main-menu">
+          <ul>
+            {
+              headerNav.map((item) => {
+                return <li key={item.id}><a href={item.link}>{item.title}</a></li>
+              })
+            }
+          </ul>
+        </nav>
+        <div
+          className="header__nav__mobile"
+          id="headerToggle"
+          role="button"
+          aria-controls="primary-menu"
+          aria-expanded={show.toString()}
+          aria-label="primary-menu"
+          tabIndex="0"
+          onClick={() => {setShow(!show);}}
+        >
+          <span></span>
+        </div>
+      </div>
+    </header>
   )
 }
 
--- src/components/Skip.jsx
+++ src/components/Skip.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Skip = () => {
   return (
-    <div>
-      Skip
+    <div id="skip">
+      <a href="#header">header</a>
+      <a href="#intro">intro</a>
+      <a href="#skill">skill</a>
+      <a href="#site">site</a>
+      <a href="#port">port</a>
+      <a href="#contact">contact</a>
+      <a href="#footer">footer</a>
     </div>
   )
 }
