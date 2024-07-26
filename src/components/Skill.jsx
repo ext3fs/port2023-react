577bbed999706d673e65d9335b7987e238870f6b
ilyoung <ext3fs@hanmail.net>
2024-07-27 06:07:07 +0900

    skill.jsx complete

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Skill = () => {
-  return (
-    <div>
-      Skill
-    </div>
-  )
-}
+  const skillText = useSelector((state) => state.skillText);
 
-export default Skill
+  return (
+    <section id="skill">
+      <div className="skill__inner">
+        <h2 className="skill__title">challenge <em>나의 도전</em></h2>
+        <div className="skill_desc">
+          {
+            skillText.map((item, key) => (
+              <div key={key}>
+                <span>{key+1}</span>
+                <h3>{item.title}</h3>
+                <p>{item.desc}</p>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Skill;
