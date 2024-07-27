a474946ee49b8acea0f8ebdcab8006ac81877afe
ilyoung <ext3fs@hanmail.net>
2024-07-28 03:46:19 +0900

    site.jsx complete

--- src/components/Site.jsx
+++ src/components/Site.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Site = () => {
+  const siteArray = useSelector((state) => state.siteArray);
+  
   return (
-    <div>
-      Site
-    </div>
-  )
-}
+    <section id="site">
+      <div className="site__inner">
+        <h2 className="site__title">site coding <em>나의 작업물</em></h2>
+        <div className="site__wrap">
+          {
+            siteArray.map((item, index) => (
+              <article className={"site__item s" + (index + 1)} key={index}>
+                <span className="num">{index + 1}.</span>
+                <div className="text">
+                  {
+                    item.text.map((text, index) => (
+                      <div key={index}>{text}</div>
+                    ))
+                  }
+                </div>
+                <h3 className="title">{item.title}</h3>
+                <div className="btn">
+                  <a href={item.code}>code</a>
+                  <a href={item.view}>view</a>
+                </div>
+                <div className="info">
+                  {
+                    item.info.map((info, index) => (
+                      <span key={index}>{info}</span>
+                    ))
+                  }
+                </div>
+              </article>
+            ))
+          }
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Site
+export default Site;
--- src/store.js
+++ src/store.js
@@ -38,6 +38,37 @@ const initialState = {
       desc: `정직은 다른 사람보다 나에게 큰 의미를 부여해야 한다.자신이 정직하지 않으면 진정으로 원하는 일을 열정적으로 밀고 나갈 수 없다.마음에서 우러나오는 일을 해야 정직해지며 삶을 더 즐길 줄 알게 된다.`
     },
   ],
+
+  siteArray: [
+    {
+      text: ['make','site compliant width','webstandard'],
+      title: 'VITE를 이용한 사이트 제작',
+      code: 'https://github.com/ext3fs/port2023-vite',
+      view: 'https://port2023-vite.netlify.app/',
+      info: ['site coding','production period : one days','use stack : html5/css3, CSS Variable, Vite']
+    },
+    {
+      text: ['make','site compliant width','webstandard'],
+      title: 'REACT를 이용한 사이트 제작',
+      code: 'https://github.com/ext3fs/port2023-react',
+      view: 'https://port2023-react.netlify.app/',
+      info: ['site coding','production period : two days','use stack : html5/css3, CSS Variable, React']
+    },
+    {
+      text: ['make','site compliant width','webstandard'],
+      title: 'VUE를 이용한 사이트 제작',
+      code: 'https://github.com/ext3fs/port2023-vue',
+      view: 'https://port2023-vue.netlify.app/',
+      info: ['site coding','production period : three days','use stack : html5/css3, CSS Variable, Vue']
+    },
+    {
+      text: ['make','site compliant width','webstandard'],
+      title: 'NEXT를 이용한 사이트 제작',
+      code: 'https://github.com/ext3fs/port2023-next',
+      view: 'https://port2023-next.netlify.app/',
+      info: ['site coding','production period : four days','use stack : html5/css3, CSS Variable, Next']
+    },
+  ],
 };
 
 const rootReducer = (state = initialState, action) => {
